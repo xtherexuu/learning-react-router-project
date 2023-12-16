21a2032c785ae32536c8d6860d8efd232be78f80
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2023-12-16 21:10:22 +0100

    changed name for 404 error page

---
+++ common/NotFoundErrorPage/index.jsx
@@ -0,0 +1,17 @@
+import { Wrapper, Button, TextElement } from "./styled";
+
+export default function NotFoundErrorPage() {
+  return (
+    <Wrapper>
+      <TextElement>
+        <span>Error 404</span>
+        <br />
+        <br />
+        Sorry, we couldn't find site what you've been searching for. 😥
+        <br />
+        Try again later o go to our homepage using button below.
+      </TextElement>
+      <Button to="/">Wróć na stronę główną</Button>
+    </Wrapper>
+  );
+}
