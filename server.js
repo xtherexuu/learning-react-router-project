6cb52ca20a0801b29127707e5611140eb87bbff9
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2023-12-16 17:46:38 +0100

    add some testing feature to our mirage js server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { createServer, Model } from "miragejs";
+import { createServer, Model, Response } from "miragejs";
 
 createServer({
   models: {
@@ -79,6 +79,7 @@ createServer({
     this.logging = false;
 
     this.get("/vans", (schema, request) => {
+      // return new Response(400, {}, {error: "Error fetching data"})
       return schema.vans.all();
     });
 
