3817b38f639b03e08339273264809dcb2af77bd3
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2024-01-03 17:34:22 +0100

    prepared Error page to one more option what can happened - when user try to find a van with id what does not exist

--- common/ErrorPage/index.jsx
+++ common/ErrorPage/index.jsx
@@ -3,16 +3,21 @@ import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-
-  console.error("Error code: " + error.status + " || Error status: " + error.statusText)
-
+  console.log(error);
+  console.error(
+    "Error code: " + error.status + " || Error status: " + error.statusText
+  );
   return (
     <Wrapper>
       <TextElement>
         <span>Error</span>
         <br />
         <br />
-        Sorry, {error.message} 😥
+        Sorry,{" "}
+        {error.message === "Cannot read properties of null (reading 'vans')"
+          ? "We can't find van with this id! Check your van id again."
+          : error.message}{" "}
+        😥
         <br />
         Try again later o go to our homepage using button below.
       </TextElement>
