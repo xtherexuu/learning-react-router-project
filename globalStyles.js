ba76b0563c912b3d6d334fb1117c9bbb1b2c936c
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2024-01-06 13:29:42 +0100

    created and applied styled-components globalStyles into project

---
+++ globalStyles.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    @keyframes loginColor {
+        0% {
+            color: rgb(255, 100, 100);
+            transform: translateX(0px);
+        }
+        20% {
+            color: red;
+        }
+        40% {
+            color: rgb(199, 0, 0);
+        }
+        80% {
+            color: rgb(255, 100, 100);
+        }
+        100% {
+            color: red;
+            transform: translateX(0px);
+        }  
+        10%, 30%, 50%, 70%, 90% {
+            transform: translateX(-5px);
+        }
+        20%, 40%, 60%, 80% {
+            transform: translateX(5px);
+        }
+    }
+`;
+
+export default GlobalStyles;
