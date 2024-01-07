6b627dfd8ea15c563151f660c4e85aee8b5ea725
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2024-01-07 11:35:07 +0100

    setup an login function in api.js and add an endpoint in our mirage js server file to login the user

--- api.js
+++ api.js
@@ -13,9 +13,29 @@ export async function fetchData(link) {
   return data;
 }
 
+export async function loginUser(creds) {
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+  return data;
+}
+
 export async function chceckIfUserIsLogedIn() {
   const isLoggedIn = false;
-  const response = redirect("/login?message=Sorry, you have to log in before you go on that page!");
+  const response = redirect(
+    "/login?message=Sorry, you have to log in before you go on that page!"
+  );
   response.body = true;
   if (!isLoggedIn) {
     throw response;
